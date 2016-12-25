2cdbb6c117e116c7db2890c3f563a173cd3b2109
zsoci <zsolt.laky@erlang-solutions.com>
2016-12-26 00:49:13 +0100

    Add normal routes to trails

--- src/cfa.erl
+++ src/cfa.erl
@@ -10,9 +10,9 @@
          start_link/0,
          stop/0]).
 
--export([process_foo/1,
-         process_too_long/1,
-         process_crashing/1]).
+-export([get_handlers/0,
+         add_routes/1,
+         add_routes/2]).
 
 
 start() -> csi:start(?SERVICE_NAME, ?SERVICE_MODULE).
@@ -20,6 +20,15 @@ start_link() -> csi:start_link(?SERVICE_NAME, ?SERVICE_MODULE).
 
 stop() -> csi:stop(?SERVICE_NAME).
 
-process_foo(Atom) -> csi:call(?SERVICE_NAME, process_foo, [Atom]).
-process_too_long(Atom) -> csi:call_p(?SERVICE_NAME, process_too_long, [Atom], 4000).
-process_crashing(Atom) -> csi:call_p(?SERVICE_NAME, process_crashing, [Atom]).
+get_handlers() -> csi:call_s(?SERVICE_NAME, get_handlers, all).
+
+add_routes(Routes) ->
+  case application:get_application() of
+    undefined ->
+      {error, app_not_defined};
+    {ok, App} ->
+      add_routes(App, Routes)
+  end.
+
+add_routes(App, Routes) ->
+  csi:call(?SERVICE_NAME, add_routes, {App, Routes}).
--- src/cfa_service.erl
+++ src/cfa_service.erl
@@ -1,6 +1,8 @@
 -module(cfa_service).
 -behaviour(csi_server).
 
+-include_lib("common_test/include/ct.hrl").
+
 %% General state of the service
 -record(cfa_state, {trail_handlers :: list(atom()),
                     pure_handlers :: dict:dict(),
@@ -11,26 +13,29 @@
                    }).
 
 %% Lifecycle State for every requests'
--record(cfa_session_state, {}).
+-record(cfa_session_state, {cfa_state :: #cfa_state{}}).
 
 -export([init_service/1,
          init/2,
          terminate/2,
-         terminate_service/2]).
+         terminate_service/2,
+         handle_call/3]).
 
 -export([trails/0,
-         process_too_long/2,
-         process_crashing/2]).
+         get_handlers/2,
+         add_routes/2]).
 
 -define(DEFAULT_NR_OF_ACCEPTORS, 5).
+-define(CFA_SERVER_REF, cfa_server).
 %% ====================================================================
 %% Behavioural functions
 %% ====================================================================
 init_service(_InitArgs) ->
   TransOpts = application:get_env(cfa, trans_opts, [{port, 8082}]),
   Acceptors = application:get_env(cfa, acceptors, ?DEFAULT_NR_OF_ACCEPTORS ),
-  State = #cfa_state{trail_handlers = [ cowboy_swagger_handler,
-                                        cfa_healthcheck_handler],
+  TrailHandlers = dict:store(cfa, [ cowboy_swagger_handler,
+                                    cfa_healthcheck_handler ], dict:new()),
+  State = #cfa_state{trail_handlers = TrailHandlers,
                      pure_handlers = dict:new(),
                      middlewares = [
                        cowboy_router,
@@ -40,11 +45,10 @@ init_service(_InitArgs) ->
                      trans_opts = TransOpts,
                      nr_of_acceptors = Acceptors
     },
-  undefined = put(dummy_handlers, State#cfa_state.pure_handlers),
-  {ok, update_routes(State)}.
+  {ok, start_server(State)}.
 
-init(_Args, _ServiceState) ->
-  {ok, #cfa_session_state{}}.
+init(_Args, ServiceState) ->
+  {ok, #cfa_session_state{cfa_state = ServiceState}}.
 
 terminate(_Reason, _State) ->
   ok.
@@ -55,43 +59,66 @@ terminate_service(_Reason, _State) ->
 %% ====================================================================
 %% Service functions
 %% ====================================================================
-process_foo(_Args, State) ->
-  {hello_world, State}.
+get_handlers(all, State = #cfa_session_state{cfa_state = CfaState}) ->
+  {[{trail_handlers, dict:to_list(CfaState#cfa_state.trail_handlers)},
+    {cowboy_handlers, dict:to_list(CfaState#cfa_state.pure_handlers)}],
+   State}.
 
-process_too_long(_Args, State) ->
-  {ok, Sleep} = application:get_env(cfa, timer_sleep),
-  timer:sleep(Sleep),
-  {long_job_finished, State}.
+add_routes({App, Routes}, State = #cfa_state{trail_handlers  = TrailHandlers,
+                                             pure_handlers   = PureHandlers,
+                                             middlewares     = Middlewares,
+                                             env             = Env}) ->
+  NewPureHandlers = dict:store(App, Routes, PureHandlers),
+  Values = set_routes(TrailHandlers, NewPureHandlers, Middlewares, Env),
+  {lists:foreach(fun({Key, Value}) ->
+                   cowboy:set_env(?CFA_SERVER_REF, Key, Value)
+                 end,
+                 Values),
+   State}.
 
-process_crashing(Args, State) ->
-  A = Args - Args,
-  {A, State}.
-
-update_routes(State = #cfa_state{trail_handlers = TrailHandlers,
-                                 middlewares = Middlewares,
-                                 env = Env,
-                                 trans_opts = TransOpts,
-                                 nr_of_acceptors = Acceptors
-  }) ->
-  TrailRoutes = trails:trails([?MODULE | TrailHandlers]),
-  trails:store(TrailRoutes),
-  Dispatch = trails:single_host_compile(TrailRoutes),
-  ProtoOpts = [{env, [{dispatch, Dispatch} | Env]},
-               {middlewares, Middlewares}],
-  case cowboy:start_http(cfa_server, Acceptors, TransOpts, ProtoOpts) of
+start_server(State = #cfa_state{trail_handlers  = TrailHandlers,
+                                pure_handlers   = PureHandlers,
+                                middlewares     = Middlewares,
+                                env             = Env,
+                                trans_opts      = TransOpts,
+                                nr_of_acceptors = Acceptors}) ->
+  ProtoOpts = set_routes(TrailHandlers, PureHandlers, Middlewares, Env),
+  case cowboy:start_http(?CFA_SERVER_REF, Acceptors, TransOpts, ProtoOpts) of
     {ok, _} -> ok;
     {error, {already_started, _}} -> ok
   end,
   State.
 
+set_routes(TrailDict, PureDict, Middlewares, Env) ->
+  _ = put(dummy_handlers, PureDict),
+  TrailHandlers = dict:fold( fun(_, Value, AccIn) ->
+                               Value ++ AccIn
+                             end, [], TrailDict),
+  TrailRoutes = trails:trails([?MODULE | TrailHandlers]),
+  ct:pal("(zsoci) ~p(~p): {TrailRoutes}:~p",
+        [?MODULE, ?LINE, {TrailRoutes}]),
+  trails:store(TrailRoutes),
+  Dispatch = trails:single_host_compile(TrailRoutes),
+  [{env, [{dispatch, Dispatch} | Env]}, {middlewares, Middlewares}].
+  
 -spec trails() -> trails:trails().
 trails() ->
-  [ make_trails(Dummy) || Dummy <- dict:to_list(get(dummy_handlers))].
+  lists:foldl(fun(Dummy, AccIn) ->
+                make_trails(Dummy) ++ AccIn
+              end, [], dict:to_list(get(dummy_handlers))).
 
+make_trails({Name, HostList}) when is_list(HostList) ->
+  lists:foldl(fun(HostRoute, AccIn) ->
+                make_trails({Name, HostRoute}) ++ AccIn
+              end, [], HostList);
 make_trails({Name, {Host, PathDefs}}) ->
   make_trails({Name, {Host, [], PathDefs}});
 make_trails({Name, {_Host, _Constraints, PathDefs}}) ->
-  [ make_a_trail(Name, Path) || Path <- PathDefs].
+  [ make_a_trail(atom_to_list(Name), Path) || Path <- PathDefs,
+                                              path_not_wildcard(Path)].
+
+path_not_wildcard({Path, _, _}) ->
+  '_' =/= Path.
 
 make_a_trail(Name, {Path, Module, _Options}) ->
   RequestBody =
@@ -104,12 +131,12 @@ make_a_trail(Name, {Path, Module, _Options}) ->
        }
     },
   MethodData =
-    #{ tags => [Name],
+    #{ tags => [Name ++ " (application)"],
        description => Name ++ " is missing swagger definition",
        parameters => [RequestBody]
     },
   Metadata =
-    #{ post => MethodData,
+    #{
        get => MethodData
     },
   Opts = #{ path => Path,
@@ -119,23 +146,8 @@ make_a_trail(Name, {Path, Module, _Options}) ->
          },
   trails:trail(Path, Module, Opts, Metadata).
 
-%%Dispatch = cowboy_router:compile([
-%%{'_', [
-%%{"/", ctr_healthcheck_handler, []},
-%%{"/favicon.ico", cowboy_static, {priv_file, ctr, "favicon.ico"}},
-%%{"/ping", ctr_healthcheck_handler, []},
-%%{"/counter/inc", ctr_counter_handler, []},
-%%{"/counter/dec", ctr_counter_handler, []},
-%%{"/counter/create", ctr_counter_handler, []},
-%%{"/counter/ix", ctr_counter_handler, []},
-%%{"/counter/[:id]", ctr_counter_handler, []},
-%%
-%%{'_', ctr_rest_handler, []}]}
-%%]),
-%%cowboy:start_http(ctr_rest_listener, 5, [{port, CowboyPort}],
-%%[{env, [{dispatch, Dispatch}]},
-%%{middlewares, [cowboy_router,
-%%cowboy_handler]}]),
-%%
-%%
-%%{'_',[],[{[],[],fen_healthcheck_handler,#{model => zsocimodel,path => "/",verbose => true}},{[<<"v1">>,<<"auth">>,<<"priv">>,<<"login">>],[],fen_auth_handler,#{function => login,model => zsocimodel,path => "/v1/auth/priv/login",verbose => false}},{[<<"v1">>,<<"auth">>,<<"priv">>,<<"register">>],[],fen_auth_handler,#{function => register,model => zsocimodel,path => "/v1/auth/priv/register",verbose => false}},{[<<"v1">>,<<"message">>,<<"get">>],[],fen_get_handler,#{function => get,model => zsocimodel,path => "/v1/message/get",verbose => false}},{[<<"v1">>,resource,function],[],fen_set_handler,#{function => set,model => zsocimodel,path => "/v1/:resource/:function",verbose => false}},{[<<"api-docs">>],[],cowboy_swagger_redirect_handler,{file,"/Users/zsoci/Projects/hive/FrontEnd/_build/default/rel/fen/lib/cowboy_swagger-1.1.0/priv/swagger/index.html"}},{[<<"api-docs">>,<<"swagger.json">>],[],cowboy_swagger_json_handler,#{}},{[<<"api-docs">>,'...'],[],cowboy_static,{dir,"/Users/zsoci/Projects/hive/FrontEnd/_build/default/rel/fen/lib/cowboy_swagger-1.1.0/priv/swagger",[{mimetypes,cow_mimetypes,all}]}}]}
+handle_call({Request, Args}, _From, State) ->
+  lager:info("Calling service through handle_call(~p)",
+             [{Request, Args}]),
+  {Reply, NewState} = ?MODULE:Request(Args, State),
+  {reply, Reply, NewState}.
